285dfbec712921e8f64c2c0138b671dd913403df
benjorama <benmacomb@gmail.com>
2023-04-24 08:39:51 -0600

    Fixes flat note text on black keys

--- src/Utils.ts
+++ src/Utils.ts
@@ -1,21 +1,17 @@
 export const getSharp = (pitch: string) => pitch.slice(0, 1) + "#" + pitch.slice(1)
 
 export const getFlat = (pitch: string, sharp: boolean) => {
-    const octave = sharp ? parseInt(pitch.slice(2, 3)) : parseInt(pitch.slice(1, 2)) 
+  const octave = sharp ? parseInt(pitch.slice(2, 3)) : parseInt(pitch.slice(1, 2))
 
-    if (pitch.includes('A')) {
-        return `Gb${octave - 1}`
-    } else if (pitch.includes('B')) {
-        return `Ab${octave}`
-    } else if (pitch.includes('C')) {
-        return `Bb${octave}`
-    } else if (pitch.includes('D')) {
-        return `Cb${octave}`
-    } else if (pitch.includes('E')) {
-        return `Db${octave}`
-    } else if (pitch.includes('F')) {
-        return `Eb${octave}`
-    } else if (pitch.includes('G')) {
-        return `Fb${octave}`
-    }
+  if (pitch.includes('A')) {
+    return `Bb${octave - 1}`
+  } else if (pitch.includes('C')) {
+    return `Db${octave}`
+  } else if (pitch.includes('D')) {
+    return `Eb${octave}`
+  } else if (pitch.includes('F')) {
+    return `Gb${octave}`
+  } else if (pitch.includes('G')) {
+    return `Ab${octave}`
+  }
 }
